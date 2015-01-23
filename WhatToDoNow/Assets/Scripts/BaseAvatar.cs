d83122d122c6163e8dc86c7e7dba677a58f0212d
Robert <robert.trypuc@gmail.com>
2015-01-23 21:09:34 +0100

    projectile added base script for charracters added avatar script modyfied

---
+++ WhatToDoNow/Assets/Scripts/BaseAvatar.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAvatar : MonoBehaviour {
+
+    public int m_HP = 100;
+    public Owner m_Owner = Owner.Enemy;
+
+    public bool facingRight = true;
+    public bool jump = false;
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public int m_meeleDamage = 10;
+    
+    public GameObject rocket;
+    public float rocketSpeed = 20f;
+    public int m_weaponDamage = 50;
+
+
+    [HideInInspector]
+    public Transform groundCheck;
+    public bool grounded = false;
+    public Animator m_anim;
+    public float m_MoveValue = 0;
+
+
+    public virtual void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
+
+
+
+    public virtual void FixedUpdate()
+    {
+        if (m_Owner != Owner.Player)
+            m_MoveValue = 0;
+        
+
+
+        if (Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+            rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+        if (m_MoveValue > 0 && !facingRight)
+            Flip();
+        else if (m_MoveValue < 0 && facingRight)
+            Flip();
+    }
+
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void FireWeapon()
+    {
+        GameObject bullet = PoolManager.Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        bullet.GetComponent<Projectile>().Spawn(m_Owner, m_weaponDamage, facingRight);
+    }
+
+
+
+    public virtual void Damage(Owner damageOwner, int damage)
+    {
+        if (damageOwner == m_Owner)
+            return;
+
+        m_HP -= damage;
+        ProcessDamage();
+    }
+
+
+
+    public virtual void ProcessDamage()
+    {
+        if (m_HP <= 0)
+        {
+            DeadAnim();
+        }
+        else
+            DamageAnim();
+    }
+
+
+
+    public virtual void DeadAnim()
+    {
+        AnimationClip ac = m_anim.animation.GetClip("Dead");
+        if (ac)
+            m_anim.Play("Dead");
+    }
+
+
+
+    public virtual void DamageAnim()
+    {
+        AnimationClip ac = m_anim.animation.GetClip("Damage");
+        if (ac)
+            m_anim.Play("Damage");
+    }
+
+}
+
+
+
+public enum Owner
+{
+    Player,
+    Enemy,
+    NPC
+}
---
+++ WhatToDoNow/Assets/Scripts/Projectile.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed = 20f;
+    public bool spawned = false;
+
+    public GameObject m_DamagePrefab;
+    public AudioClip m_destoryClip;
+
+    public float m_TimeToDestory = 10f;
+
+    Owner owner = Owner.Enemy;
+    int damage = 10;
+
+    float time = 0;
+
+    
+
+    public void Spawn(Owner owner, int damage, bool facingRight)
+    {
+        this.owner = owner;
+        this.damage = damage;
+
+        if (!facingRight)
+            rigidbody2D.velocity = new Vector2(-speed, 0);
+        else
+            rigidbody2D.velocity = new Vector2(speed, 0);
+
+    }
+
+    void Start () 
+    {
+        spawned = false;
+        time = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        BaseAvatar avatar = coll.transform.GetComponent<BaseAvatar>();
+        if (avatar)
+        {
+            avatar.Damage(owner, damage);
+        }
+        ProcessDestory();
+    }
+
+    void ProcessDestory()
+    {
+        if (m_DamagePrefab)
+            PoolManager.Instantiate(m_DamagePrefab, transform.position, Quaternion.identity);
+        if (m_destoryClip)
+            AudioSource.PlayClipAtPoint(m_destoryClip, transform.position);
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= m_TimeToDestory)
+            ProcessDestory();
+    }
+}
