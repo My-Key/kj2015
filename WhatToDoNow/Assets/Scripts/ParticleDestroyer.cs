cce087a75c1024b187b5621b03705fd059b5022c
Robert <robert.trypuc@gmail.com>
2015-01-24 05:39:36 +0100

    Board game

---
+++ WhatToDoNow/Assets/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDestroyer : MonoBehaviour {
+
+    public float m_liveTime = 3f;
+    float m_time = 0;
+
+    bool isAlive = false;
+	// Use this for initialization
+	void Start () 
+    {
+        isAlive = true;
+        m_time = m_liveTime;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!isAlive)
+            return;
+
+        m_time -= Time.deltaTime;
+        if (m_time <= 0)
+        {
+            isAlive = false;
+            PoolManager.Destroy(this.gameObject);
+        }
+	}
+}
---
+++ WhatToDoNow/Assets/Scripts/PlayerReseter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerReseter : MonoBehaviour 
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        BaseAvatar avatar = other.transform.GetComponent<BaseAvatar>();
+        if (avatar)
+        {
+            if (avatar.m_Owner == Owner.Player)
+            {
+                GameLoader.instance.ResetPlayer();
+            }
+            else
+                PoolManager.Destroy(avatar.gameObject);
+            
+        }
+    }
+}
